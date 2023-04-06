ae9f672ef935152dd709c7341325728ea26d82c4
dimpai <129895496+dimpai@users.noreply.github.com>
2023-04-06 11:37:42 -0400

    made customer account creation

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 public class Customer {
+    private String username;
+    private String password;
+    public Customer(String username, String password, boolean newUser) {
+        if (newUser) {
+            this.username = username;
+            this.password = password;
+            writeAccount();
+        }
+
+    }
+
+    public void writeAccount() {
+        File f = new File("customers.txt");
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(f, true))) {
+            pw.println(username + ":" + password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Market.java
+++ src/Market.java
@@ -1,12 +1,36 @@
 import java.util.Scanner;
 public class Market {
     public static void main(String[] args) {
-        // logging in
+        // logging in/creating account
         Scanner scanner = new Scanner(System.in);
+        String userType;
+        String accountType; // create an account or login
+        String username;
+        String password;
         // add name if we want
         System.out.println("Welcome to the marketplace!");
         System.out.println("Please select your account type");
-        System.
+        do {
+            System.out.print("1. Seller\n2. Customer\n");
+            userType = scanner.nextLine();
+            if (!userType.equals("1") && !userType.equals("2"))
+                System.out.println("Please enter 1 for seller or 2 for customer");
+        } while (!userType.equals("1") && !userType.equals("2"));
 
+        do {
+            System.out.print("1. Login\n2. Create an account\n");
+            accountType = scanner.nextLine();
+            if (!accountType.equals("1") && !accountType.equals("2"))
+                System.out.println("Please enter 1 to login or 2 to create an account");
+        } while (!accountType.equals("1") && !accountType.equals("2"));
+
+        // creating account
+        if (accountType.equals("2")) {
+            System.out.println("Please enter a username/email");
+            username = scanner.nextLine();
+            System.out.println("Please enter a password");
+            password = scanner.nextLine();
+            Customer customer = new Customer(username, password, true);
+        }
     }
 }
