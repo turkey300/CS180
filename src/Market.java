53cc949f6684ca485d7cfd25eaba1d1b0d21936f
dimpai <129895496+dimpai@users.noreply.github.com>
2023-04-06 10:46:09 -0400

    starting code

--- src/Market.java
+++ src/Market.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Market {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // logging in
+        Scanner scanner = new Scanner(System.in);
+        // add name if we want
+        System.out.println("Welcome to the marketplace!");
+        System.out.println("Please select your account type");
+        System.
 
     }
 }
